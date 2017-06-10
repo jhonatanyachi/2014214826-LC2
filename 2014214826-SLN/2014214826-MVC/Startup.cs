3093ed4cb6f2369403a05cf47d966a90bd5cf762
jhonatanyachi <jhulian7_4@hotmail.com>
2017-06-10 07:55:58 -0500

    2014214826

---
+++ 2014214826-SLN/2014214826-MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_2014214826_MVC.Startup))]
+namespace _2014214826_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
